48408eef4877f4ae3e2f83203b3b27706f0e3329
uncleJamal <R.Mihail@student.tudelft.nl>
2024-08-10 17:19:24 +0200

    created some crucial logic and some tests
    so far, the problem is solved (and tested) analytically, not with the goal seek algorithm.

---
+++ Solution.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+
+namespace dentsu
+{
+    /// <summary>
+    /// This is a class that contains all the data and the business logic needed to solve the problem.
+    /// </summary>
+    public class Solution
+    {
+        private double _totalBudget; //Z
+        private readonly Ad[] _ads; //X
+        private double _agencyFee; //Y1
+        private double _thirdPartyFee; //Y2
+        private double _workingHoursFee; //HOURS
+        private int _targetAd; //ad whose budget must be maximized
+
+        /// <summary>
+        /// constructor for a solution.
+        /// </summary>
+        /// <param name="totalBudget"></param>
+        /// <param name="ads"></param>
+        /// <param name="agencyFee"></param>
+        /// <param name="thirdPartyFee"></param>
+        /// <param name="workingHoursFee"></param>
+        /// <param name="targetAd"></param>
+        public Solution(double totalBudget, Ad[] ads, double agencyFee, double thirdPartyFee, double workingHoursFee, int targetAd)
+        {
+            this._totalBudget = totalBudget;
+            this._ads = ads;
+            this._agencyFee = agencyFee;
+            this._thirdPartyFee = thirdPartyFee;
+            this._workingHoursFee = workingHoursFee;
+            this._targetAd = targetAd;
+        }
+        
+        //TODO: create getters/setters for all properties
+        /// <summary>
+        /// Returns the allocated fee for the target ad.
+        /// </summary>
+        /// <returns> (double) value of target ad fee</returns>
+        public double GetTargetAdFee()
+        {
+            return this._ads[_targetAd].Fee;
+        }
+        
+        /// <summary>
+        /// Computes whether the budget is exceeded.
+        /// </summary>
+        /// <returns>true for budget exceeded, false otherwise</returns>
+        public bool ExceedsBudget()
+        {
+            double totalAdFees = this._ads.Sum(ad => ad.Fee);
+            double totalThirdPartyFees = this._ads.Where(ad => ad.Enhanced).Sum(ad => ad.Fee);
+            double expendedBudgetSoFar = totalAdFees + _agencyFee * totalAdFees + _thirdPartyFee * totalThirdPartyFees + _workingHoursFee;
+            return expendedBudgetSoFar > this._totalBudget;
+        }
+        /// <summary>
+        /// Computes the exact maximum fee that can be allocated to the target ad.
+        /// There are 2 distinct formulas, which take into account whether the target ad will be enhanced or not.
+        /// This method is used exclusively for testing purposes, and has no bearing on the actual algorithm.
+        /// </summary>
+        public void AnalyticalSolution()
+        {
+            double result = 0.0;
+            double feeSum = this._ads.Sum(ad => ad.Fee); // sum of the fees of all the ads except the target ad
+            double enhancedSum = this._ads.Where(ad => ad.Enhanced).Sum(ad => ad.Fee); //sum of the fees for the enhanced ads
+            if (!this._ads[this._targetAd].Enhanced)
+            {
+                //TODO: throw exception if totalBudget is exceeded
+                result = (_totalBudget - feeSum - _agencyFee * feeSum - _thirdPartyFee * enhancedSum - _workingHoursFee) / (1.0 + _agencyFee);
+            }
+            else
+            {
+                //TODO: throw exception if totalBudget is exceeded
+                result = (_totalBudget - feeSum - _agencyFee * feeSum - _thirdPartyFee * enhancedSum - _workingHoursFee) / (1.0 + _agencyFee + _thirdPartyFee);
+            }
+            this._ads[_targetAd].Fee = result;
+        }
+        
+        /// <summary>
+        /// getters and setters for all attributes
+        /// </summary>
+        public double TotalBudget
+        {
+            get { return _totalBudget; }
+            set { _totalBudget = value; }
+        }
+
+        public double AgencyFee
+        {
+            get { return _agencyFee; }
+            set { _agencyFee = value; }
+        }
+
+        public double ThirdPartyFee
+        {
+            get { return _thirdPartyFee; }
+            set { _thirdPartyFee = value; }
+        }
+
+        public double WorkingHoursFee
+        {
+            get { return _workingHoursFee; }
+            set { _workingHoursFee = value; }
+        }
+
+        public int TargetAd
+        {
+            get { return _targetAd; }
+            set { _targetAd = value; }
+        }
+    }
+}
---
+++ SolutionTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using dentsu;
+using NUnit.Framework;
+
+namespace dentsu
+{
+    [TestFixture]
+    [TestOf(typeof(Solution))]
+    public class SolutionTest
+    {
+
+        [Test]
+        public void test_analytical_method_1()
+        {
+            Ad ad0 = new Ad("X0", 0, false);
+            Ad ad1 = new Ad("X1", 200.0, true);
+            Ad ad2 = new Ad("X2", 700.0, true);
+            Ad ad3 = new Ad("X3", 1000, true);
+            Ad ad4 = new Ad("X4", true);
+
+            Ad[] ads = new Ad[] {ad0, ad1, ad2, ad3, ad4};
+
+            double totalBudget = 5000.0;
+            double agencyFee = 0.1;
+            double thirdPartyFee = 0.2;
+            double workingHoursFee = 500;
+            int targetAd = 4;
+
+            Solution sol = new Solution(totalBudget, ads, agencyFee, thirdPartyFee, workingHoursFee, targetAd);
+            sol.AnalyticalSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+        }
+    }
+}
