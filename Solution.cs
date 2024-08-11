5e83e6798cd29d5341dfe875ef38ab5773143326
uncleJamal <R.Mihail@student.tudelft.nl>
2024-08-11 22:35:33 +0200

    created the main goal seek algorithm
    also added some tests and input validation

--- Solution.cs
+++ Solution.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Drawing;
+using System.Linq;
 
 namespace dentsu
 {
@@ -13,6 +14,7 @@ namespace dentsu
         private double _thirdPartyFee; //Y2
         private double _workingHoursFee; //HOURS
         private int _targetAd; //ad whose budget must be maximized
+        private int _numberOfAds;
 
         /// <summary>
         /// constructor for a solution.
@@ -31,6 +33,7 @@ namespace dentsu
             this._thirdPartyFee = thirdPartyFee;
             this._workingHoursFee = workingHoursFee;
             this._targetAd = targetAd;
+            this._numberOfAds = _ads.Length;
         }
         
         //TODO: create getters/setters for all properties
@@ -42,17 +45,52 @@ namespace dentsu
         {
             return this._ads[_targetAd].Fee;
         }
-        
+
         /// <summary>
-        /// Computes whether the budget is exceeded.
+        /// Computes the total budget that has been spent so far.
         /// </summary>
-        /// <returns>true for budget exceeded, false otherwise</returns>
-        public bool ExceedsBudget()
+        /// <returns></returns>
+        public double ExpendedBudgetSoFar()
         {
             double totalAdFees = this._ads.Sum(ad => ad.Fee);
             double totalThirdPartyFees = this._ads.Where(ad => ad.Enhanced).Sum(ad => ad.Fee);
             double expendedBudgetSoFar = totalAdFees + _agencyFee * totalAdFees + _thirdPartyFee * totalThirdPartyFees + _workingHoursFee;
-            return expendedBudgetSoFar > this._totalBudget;
+            return expendedBudgetSoFar;
+        }
+        /// <summary>
+        /// Computes whether the total budget is exceeded.
+        /// </summary>
+        /// <returns>true for budget exceeded, false otherwise</returns>
+        public bool ExceedsBudget()
+        {
+            return this.ExpendedBudgetSoFar() > this._totalBudget;
+        }
+
+        public bool IsWithinThreshold(double expendedBudgetSoFar, double threshold)
+        {
+            return this._totalBudget - expendedBudgetSoFar <= threshold;
+        }
+        
+        /// <summary>
+        /// Using the idea behind a goal seek algorithm, this method finds the maximum budget that can be allotted to the target ad.
+        /// Uses a threshold for when the algorithm should stop, as well as a multiplier that affects the size of the steps taken towards convergence.
+        /// </summary>
+        public void GoalSeekSolution()
+        {
+            double threshold = 5.0;
+            double multiplier = 0.7;
+            double expendedBudgetSoFar = this.ExpendedBudgetSoFar();
+            Ad targetAd = this._ads[this._targetAd];
+            while (!IsWithinThreshold(expendedBudgetSoFar, threshold) && multiplier > 0.0)
+            {
+                expendedBudgetSoFar = this.ExpendedBudgetSoFar();
+                if (expendedBudgetSoFar < this._totalBudget)
+                    targetAd.Fee += (this._totalBudget - expendedBudgetSoFar) * multiplier;
+                else
+                    targetAd.Fee -= (this._totalBudget - expendedBudgetSoFar) * multiplier;
+                multiplier -= 0.02;
+            }
+            Console.WriteLine("done!");
         }
         /// <summary>
         /// Computes the exact maximum fee that can be allocated to the target ad.
@@ -76,6 +114,11 @@ namespace dentsu
             }
             this._ads[_targetAd].Fee = result;
         }
+
+        public void Reset()
+        {
+            this._ads[this._targetAd].Fee = 0.0;
+        }
         
         /// <summary>
         /// getters and setters for all attributes
@@ -83,31 +126,66 @@ namespace dentsu
         public double TotalBudget
         {
             get { return _totalBudget; }
-            set { _totalBudget = value; }
+            set
+            {
+                if (value is double && value >= 0.0)
+                    _totalBudget = value;
+                else
+                    throw new ArgumentOutOfRangeException("Total budget must be a rational number greater than 0.");
+            }
         }
 
         public double AgencyFee
         {
             get { return _agencyFee; }
-            set { _agencyFee = value; }
+            set
+            {
+                if (value is double && value >= 0.0 && value <= 1.0)
+                    _agencyFee = value;
+                else
+                    throw new ArgumentOutOfRangeException("Agency fee must be a fraction between 0 and 1.");
+            }
         }
 
         public double ThirdPartyFee
         {
             get { return _thirdPartyFee; }
-            set { _thirdPartyFee = value; }
+            set       
+            {
+                if (value is double && value >= 0.0 && value <= 1.0)
+                    _thirdPartyFee = value;
+                else
+                    throw new ArgumentOutOfRangeException("Third party fee must be a fraction between 0 and 1.");
+            }
         }
 
         public double WorkingHoursFee
         {
             get { return _workingHoursFee; }
-            set { _workingHoursFee = value; }
+            set
+            {
+                if (value is double && value >= 0.0 && value <= _totalBudget)
+                    _workingHoursFee = value;
+                else
+                    throw new ArgumentOutOfRangeException("Working hours fee must be a rational number greater than 0 and smaller than the total budget.");
+            }
         }
 
         public int TargetAd
         {
             get { return _targetAd; }
-            set { _targetAd = value; }
+            set
+            {
+                if (value is double && value >= 0 && value <= _numberOfAds)
+                    _totalBudget = value;
+                else
+                    throw new ArgumentOutOfRangeException("The index of the target ad must be an integer between 1 and the number of total advertisements.");
+            }
+        }
+
+        public int NumberOfAds
+        {
+            get { return _numberOfAds; }
         }
     }
 }
--- SolutionTest.cs
+++ SolutionTest.cs
@@ -30,5 +30,121 @@ namespace dentsu
             sol.AnalyticalSolution();
             Console.WriteLine(sol.GetTargetAdFee());
         }
+        [Test]
+        public void test_goal_seek_method_1()
+        {
+            Ad ad0 = new Ad("X0", 0, false);
+            Ad ad1 = new Ad("X1", 200.0, true);
+            Ad ad2 = new Ad("X2", 700.0, true);
+            Ad ad3 = new Ad("X3", 1000, true);
+            Ad ad4 = new Ad("X4", true);
+
+            Ad[] ads = new Ad[] {ad0, ad1, ad2, ad3, ad4};
+
+            double totalBudget = 5000.0;
+            double agencyFee = 0.1;
+            double thirdPartyFee = 0.2;
+            double workingHoursFee = 500;
+            int targetAd = 4;
+
+            Solution sol = new Solution(totalBudget, ads, agencyFee, thirdPartyFee, workingHoursFee, targetAd);
+            sol.GoalSeekSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+            sol.Reset();
+            sol.AnalyticalSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+        }
+        [Test]
+        public void test_goal_seek_method_2()
+        {
+            Ad ad0 = new Ad("Y0", 100, false);
+            Ad ad1 = new Ad("Y1", 300.0, true);
+            Ad ad2 = new Ad("Y2", 800.0, true);
+
+            Ad[] ads = new Ad[] { ad0, ad1, ad2 };
+
+            double totalBudget = 10000.0;
+            double agencyFee = 0.15;
+            double thirdPartyFee = 0.25;
+            double workingHoursFee = 800;
+            int targetAd = 2;
+
+            Solution sol = new Solution(totalBudget, ads, agencyFee, thirdPartyFee, workingHoursFee, targetAd);
+            sol.GoalSeekSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+            sol.Reset();
+            sol.AnalyticalSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+        }
+        [Test]
+        public void test_goal_seek_method_3()
+        {
+            Ad ad0 = new Ad("Z0", 50, false);
+            Ad ad1 = new Ad("Z1", 150.0, true);
+            Ad ad2 = new Ad("Z2", 400.0, false);
+            Ad ad3 = new Ad("Z3", 600, true);
+            Ad ad4 = new Ad("Z4", 250.0, false);
+            Ad ad5 = new Ad("Z5", 500.0, true);
+
+            Ad[] ads = new Ad[] { ad0, ad1, ad2, ad3, ad4, ad5 };
+
+            double totalBudget = 3000.0;
+            double agencyFee = 0.1;
+            double thirdPartyFee = 0.2;
+            double workingHoursFee = 300;
+            int targetAd = 5;
+
+            Solution sol = new Solution(totalBudget, ads, agencyFee, thirdPartyFee, workingHoursFee, targetAd);
+            sol.GoalSeekSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+            sol.Reset();
+            sol.AnalyticalSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+        }
+        [Test]
+        public void test_goal_seek_method_4()
+        {
+            Ad ad0 = new Ad("A0", 75, false);
+            Ad ad1 = new Ad("A1", 250.0, true);
+            Ad ad2 = new Ad("A2", 450.0, false);
+            Ad ad3 = new Ad("A3", 350.0, true);
+            Ad ad4 = new Ad("A4", 550.0, false);
+
+            Ad[] ads = new Ad[] { ad0, ad1, ad2, ad3, ad4 };
+
+            double totalBudget = 7000.0;
+            double agencyFee = 0.12;
+            double thirdPartyFee = 0.18;
+            double workingHoursFee = 400;
+            int targetAd = 3;
+
+            Solution sol = new Solution(totalBudget, ads, agencyFee, thirdPartyFee, workingHoursFee, targetAd);
+            sol.GoalSeekSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+            sol.Reset();
+            sol.AnalyticalSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+        }
+        [Test]
+        public void test_goal_seek_method_5()
+        {
+            Ad ad0 = new Ad("B0", 50, true);
+
+            Ad[] ads = new Ad[] { ad0 };
+
+            double totalBudget = 1000.0;
+            double agencyFee = 0.05;
+            double thirdPartyFee = 0.1;
+            double workingHoursFee = 100;
+            int targetAd = 0;
+
+            Solution sol = new Solution(totalBudget, ads, agencyFee, thirdPartyFee, workingHoursFee, targetAd);
+            sol.GoalSeekSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+            sol.Reset();
+            sol.AnalyticalSolution();
+            Console.WriteLine(sol.GetTargetAdFee());
+        }
+
     }
 }
